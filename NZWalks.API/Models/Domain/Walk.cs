e78f18ae376dcc29c35da83db13bb408dfc55d09
Emanuel <emanuel.fernandesdasilva@gmail.com>
2024-08-08 18:49:00 -0300

    Adicionar arquivos de projeto.

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,25 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+	public class Walk
+	{
+		public Guid Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public double LenghInKm { get; set; }
+
+		public string WalkImageUrl { get;}
+
+		public Guid DifficultyId { get; set; }
+
+		public Guid RegionId { get; set; }
+
+
+		//navigation properties
+		public Difficulty Difficulty { get; set; }
+
+		public Difficulty Region { get; set; }
+	}
+}
